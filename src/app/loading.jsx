d35d8479ed00a9e4b3bc9fa26a83f8824f855933
muharriss <muhharris@gmail.com>
2025-01-03 23:17:06 +0700

    add loading screen

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center w-screen h-[100vh] overflow-hidden" >
+            <div className="w-12 h-12 border-4 border-t-black border-r-transparent rounded-full animate-spin"></div>
+        </div>
+    )
+}
+
+export default Loading
