49c28412738a87105ea64691e9e0ba18951135f4
muharriss <muhharris@gmail.com>
2025-01-07 21:23:20 +0700

    tes

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     const [isBlurred, setIsBlurred] = useState(false);
     const [initialized, setInitialized] = useState(false);
     const [toggle, setToggle] = useState(false)
-    const [isDelay, setIsDelay] = useState(true)
+    // const [isDelay, setIsDelay] = useState(true)
 
     useEffect(() => {
 
@@ -45,25 +45,25 @@ const Home = () => {
     }, [toggle]);
 
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsDelay(false);
-        }, 5000); 
-        return () => clearTimeout(timer); // Bersihkan timer jika komponen unmount
-    }, []);
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setIsDelay(false);
+    //     }, 5000); 
+    //     return () => clearTimeout(timer); // Bersihkan timer jika komponen unmount
+    // }, []);
 
 
     return (
         <div >
             <Navbar isBlurred={isBlurred} toggle={toggle} setToggle={setToggle} />
             <SideBar toggle={toggle} setToggle={setToggle} />
-            <div className={`fixed ${isDelay ? "h-[800px]" : "h-screen"} sm:h-screen w-full -z-10 bg-[url('/bg.webp')] bg-cover bg-center`} >
+            <div className={`fixed h-dvh w-full -z-10 bg-[url('/bg.webp')] bg-cover bg-center`} >
                 <div className={`h-full flex flex-col justify-center items-center  `}>
                     <p className="text-7xl sm:text-8xl md:text-[8rem] xl:text-[10rem] text-nowrap text-center">harris</p>
                     <p className="text-3xl text-center text-nowrap">Frontend Developer</p>
                 </div>
             </div>
-            <div className={`fixed h-screen w-full -z-10 ${!initialized ? "backdrop-blur-md bg-black bg-opacity-[82%] " : ""}  ${isBlurred ? "backdrop-blur-md bg-black bg-opacity-[82%]  " : ""}  transition-all duration-300`} />
+            <div className={`fixed h-dvh w-full -z-10 ${!initialized ? "backdrop-blur-md bg-black bg-opacity-[82%] " : ""}  ${isBlurred ? "backdrop-blur-md bg-black bg-opacity-[82%]  " : ""}  transition-all duration-300`} />
         </div>
     )
 }
