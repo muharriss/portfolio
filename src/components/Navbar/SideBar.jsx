2409496f41c09c8388a90757a15386178ed1c2bf
muharriss <muhharris@gmail.com>
2025-01-07 20:49:54 +0700

    tes

--- src/components/Navbar/SideBar.jsx
+++ src/components/Navbar/SideBar.jsx
@@ -7,16 +7,16 @@ import { MdEmail } from "react-icons/md";
 const SideBar = ({ toggle, setToggle }) => {
 
     return (
-        <div className={`fixed max-w-5xl w-screen h-screen bg-transparent flex z-20  ${toggle ? "block" : "hidden"} `}>
+        <div className={`fixed max-w-5xl w-screen h-dvh bg-transparent flex z-20  ${toggle ? "block" : "hidden"} `}>
 
-            <div className="relative p-6 w-1/2 h-screen bg-white dark:bg-black ">
+            <div className="relative p-6 w-1/2 h-dvh bg-white dark:bg-black ">
                 <button onClick={() => setToggle(!toggle)} className="float-right text-xl hover:scale-[120%]  rounded transition-all duration-300 px-1">X</button>
                 <div className="flex flex-col pt-6 space-y-4 text-xl ">
                     <Link href="#about" onClick={() => setToggle(!toggle)} className="hover:text-[#1e88e5] transition-all duration-300 ease-in-out ">About</Link>
                     <Link href="#projects" onClick={() => setToggle(!toggle)} className="hover:text-[#1e88e5] transition-all duration-300 ease-in-out">Projects</Link>
                     <Link href="#contact" onClick={() => setToggle(!toggle)} className="hover:text-[#1e88e5] transition-all duration-300 ease-in-out">Contact</Link>
                 </div>
-                <div className="gap-3 flex justify-center absolute w-full h-full top-0 left-0 items-end pb-10">
+                <div className="gap-3 flex justify-center absolute w-full h-dvh top-0 left-0 items-end pb-10 ">
                     <a href="mailto:muhharris04@gamil.com" aria-label="Email" >
                         <MdEmail className="cursor-pointer size-10 p-2 rounded-md hover:bg-white hover:text-black transition-all duration-300 ease-in-out" />
                     </a>
