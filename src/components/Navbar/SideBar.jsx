9686a41fd8df8c5d73c75728fa2903a21d16ff84
muharriss <muhharris@gmail.com>
2025-01-07 21:18:03 +0700

    change h-screen to h-dvh

--- src/components/Navbar/SideBar.jsx
+++ src/components/Navbar/SideBar.jsx
@@ -7,9 +7,9 @@ import { MdEmail } from "react-icons/md";
 const SideBar = ({ toggle, setToggle }) => {
 
     return (
-        <div className={`fixed max-w-5xl w-screen h-screen bg-transparent flex z-20  ${toggle ? "block" : "hidden"} `}>
+        <div className={`fixed max-w-5xl w-screen h-dvh bg-transparent flex z-20  ${toggle ? "block" : "hidden"} `}>
 
-            <div className="relative p-6 w-1/2 h-screen bg-white dark:bg-black ">
+            <div className="relative p-6 w-1/2 h-dvh bg-white dark:bg-black ">
                 <button onClick={() => setToggle(!toggle)} className="float-right text-xl hover:scale-[120%]  rounded transition-all duration-300 px-1">X</button>
                 <div className="flex flex-col pt-6 space-y-4 text-xl ">
                     <Link href="#about" onClick={() => setToggle(!toggle)} className="hover:text-[#1e88e5] transition-all duration-300 ease-in-out ">About</Link>
